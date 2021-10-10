744fb7872f0738e7e657f2f37fd6571e81b754dc
LENOVO <piotrrrynkowskii@gmail.com>
2021-10-10 18:31:19 +0200

    First exercise

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-//        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
+        System.out.println("Hello World");
+        System.out.println("To jest nasz pierwszy program w jezuku java");
+        System.out.println("zycze"+ " "+ "wam "+"milej nauki !");
 
+        System.out.printf("A %s zaczynamy z %d przykladem.\n", "wiec",1);
+
+        System.out.print("to jakis teskt");
+        System.out.print(" tekst");
+        System.out.print("nie za dlugi");
 
     }
 }
