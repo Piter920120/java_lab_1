1260addcb7a4b8bc3748de054af3ec332bca678d
LENOVO <piotrrrynkowskii@gmail.com>
2021-10-10 19:56:06 +0200

    Second exercise

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,15 @@ public class Main {
         System.out.println("zmienna int = " + zmiennaInt);
         /* ćw.2 */
 
+
+        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+
+        /*
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
         int zmienna1 = 16;
         String imie1 = "Piotr";
         double wiek = 27.11;
