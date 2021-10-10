8b017f21fa36cb0f2369b910687616dfb25dd2ed
LENOVO <piotrrrynkowskii@gmail.com>
2021-10-10 19:24:43 +0200

    Second exercise

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+
         /* ćw.3 */
 
         /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
@@ -12,10 +14,50 @@ public class Main {
         *   b) wyświetlić dane za pomocą println i printf
         */
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Poodaj Imie: ");
+        String imie = scan.next();
+
+        System.out.print("Poodaj Nazwisko: ");
+        String nazwisko = scan.next();
+
+        System.out.print("wiek: ");
+        int wiek = scan.nextInt();
+
+        System.out.print("Podaj nr indeksu: ");
+        int nrIndexu = scan.nextInt();
+
+
+        System.out.println("wiek: "+ wiek);
+
+        System.out.printf("%s ,%s ", imie , nazwisko);
+        System.out.println("wiek: "+ wiek);
+        System.out.println("nr indeksu: "+nrIndexu);
+
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+
+        System.out.print("podaj pierwsza liczbe: ");
+        int liczba1 = scan.nextInt();
+
+        System.out.print("Podaj druga liczbe: ");
+        int liczba2 = scan.nextInt();
+
+
+
+        System.out.println(liczba1+liczba2);
+        System.out.println(liczba1 - liczba2);
+        System.out.println(liczba1 * liczba2);
+        System.out.println(liczba2/liczba1);
+        System.out.println(liczba1%liczba2);
+
+
+
+
+
 
     }
 }
