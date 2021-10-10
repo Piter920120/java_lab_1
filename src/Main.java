ff8b3fbf0e7ec7108d913b2532da60a6fe45dad1
LENOVO <piotrrrynkowskii@gmail.com>
2021-10-10 19:00:15 +0200

    Second exercise

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.PipedOutputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +9,38 @@ public class Main {
         System.out.println("zmienna int = " + zmiennaInt);
         /* ćw.2 */
 
-        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        int zmienna1 = 16;
+        String imie1 = "Piotr";
+        double wiek = 27.11;
+        boolean zmiennaBoolean = true;
 
+        System.out.println(imie1);
+        System.out.println(zmienna1);
+        System.out.println(wiek);
+        System.out.print(zmiennaBoolean);
+
+
+
+        //cw2
+
+        int liczbaA = 7;
+        int liczbaB = 10;
+
+        double liczbaX = 12.5;
+        double liczbaY = 20.3;
+
+
+        System.out.println(liczbaA + liczbaB);
+        System.out.println(liczbaA - liczbaB);
+        System.out.println(liczbaA * liczbaB);
+        System.out.println(liczbaA/liczbaB);
+        System.out.println(liczbaB%liczbaA);
+
+        System.out.println(liczbaX + liczbaY);
+        System.out.println(liczbaX - liczbaY);
+        System.out.println(liczbaX * liczbaY);
+        System.out.println(liczbaY/liczbaX);
+        System.out.println(liczbaY%liczbaX);
         /*
         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
         *    mnożenia, dziealenia i modulo na zmiennych:
